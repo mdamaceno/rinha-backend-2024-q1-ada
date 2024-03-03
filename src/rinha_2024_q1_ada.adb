2ce83360d41cbd61eba4c3dde83ff3c9c388c3b6
Marco Damaceno <maadamaceno@gmail.com>
2024-03-03 17:34:45 -0300

    fix: error on restarting the server

--- src/rinha_2024_q1_ada.adb
+++ src/rinha_2024_q1_ada.adb
@@ -2,20 +2,22 @@ with Ada.Text_IO;
 with AWS.Server;
 with AWS.Server.Log;
 with AWS.Log;
+with AWS.Config;
+with AWS.Config.Set;
 
 with Api_CB;
 
 procedure Rinha_2024_Q1_Ada is
 
    WS : AWS.Server.HTTP;
+   AWS_Config      : AWS.Config.Object := AWS.Config.Default_Config;
 
 begin
-   Ada.Text_IO.Put_Line ("AWS " & AWS.Version);
+   AWS.Config.Set.Reuse_Address (AWS_Config, True);
+   AWS.Config.Set.Server_Port (AWS_Config, 8080);
 
-   AWS.Server.Start
-     (WS, "Rinha de Backend Q1 2024",
-      Callback => Api_CB.Service'Access,
-      Port => 8080);
+   Ada.Text_IO.Put_Line ("AWS " & AWS.Version);
+   AWS.Server.Start (WS, Callback => Api_CB.Service'Access, Config => AWS_Config);
 
    AWS.Server.Log.Start (WS, Split_Mode => AWS.Log.Daily);
 
